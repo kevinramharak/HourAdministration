4836285d9cdf9418d5d853525a427b5ccfe92806
justusj94 <justusjansen94@gmail.com>
2016-03-23 16:02:04 +0100

    nav werkt nu echt

--- Web/php/navs/fellowworker.php
+++ Web/php/navs/fellowworker.php
@@ -9,12 +9,11 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">HourAdministration</a>
+            <a class="navbar-brand" href="../fellow-worker/home.php"">HourAdministration</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li><a href="../fellow-worker/home.php">Home</a></li>
                 <li><a href="../fellow-worker/vacation.php">Vrij aanvragen</a></li>
                 <li><a href="../fellow-worker/sick.php">Ziek melden</a></li>
                 <li><a href="../fellow-worker/hours.php">Uren registreren</a></li>
