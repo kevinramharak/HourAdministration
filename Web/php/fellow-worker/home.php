1cb384393324e322b27a060e42e0efbbc1adcc05
justusj94 <justusjansen94@gmail.com>
2016-03-23 22:28:18 +0100

    home aangepast van medewerker

--- Web/php/fellow-worker/home.php
+++ Web/php/fellow-worker/home.php
@@ -4,12 +4,28 @@
     <div class="container">
         <div class="col-xs-12">
             <div class="row">
-                <?php include_once '../navs/fellowworker.php'; ?>
-            </div>
-        </div>
-        <div class="col-xs-12">
-            <div class="row">
-                <h1>Welkom!</h1>
+                <div class="col-xs-12 col-md-6">
+                    <a href="vacation.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton" href="vacation.php">Vrij aanvragen</button></a>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <a href="sick.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton">Ziek melden</button></a>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <a href="hours.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton">Uren registreren</button></a>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <a href="overtime.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton">Overwerk registreren</button></a>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <a href="agenda.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton">Agenda</button></a>
+                </div>
+                <div class="col-xs-12 col-md-6">
+                    <a href="summary.php" class="menuButton"><button type="button" class="btn btn-default btn-lg btn-block menuButton">Overzicht</button></a>
+                </div>
+                <div class="col-xs-12">
+                    <h1>Welkom!</h1>
+                    <p>Welkom medewerker, vanuit hier kunt u navigeren naar de verschillende onderdelen van de site.</p>
+                </div>
             </div>
         </div>
     </div>
