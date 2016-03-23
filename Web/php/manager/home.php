3a96f65163818f1ecd5a171b092b0b11b367a38f
justusj94 <justusjansen94@gmail.com>
2016-03-23 17:37:04 +0100

    aanpassingen front shit

--- Web/php/manager/home.php
+++ Web/php/manager/home.php
@@ -2,16 +2,7 @@
 <?php include_once '../head.php'; ?>
 <body>
 <div class="container">
-    <div class="col-xs-10 col-xs-offset-1 col-md-1 col-md-offset-0">
-        <div class="row">
-            <img src="../../img/logo.png" class="logo">
-        </div>
-    </div>
-    <div class="col-xs-12">
-        <div class="row">
-            <?php include_once '../navs/manager.php'; ?>
-        </div>
-    </div>
+    <?php include_once '../navs/manager.php'; ?>
     <div class="col-xs-12">
         <div class="row">
             <h1>Welkom!</h1>
