b189fdc4a7036ee575cbfbe9ac6dfa165198a507
Tedderksen <t.derksen@hotmail.com>
2016-03-23 15:41:03 +0100

    teun

---
+++ Web/php/manager/home.php
@@ -0,0 +1,22 @@
+<html>
+<?php include_once '../head.php'; ?>
+<body>
+<div class="container">
+    <div class="col-xs-10 col-xs-offset-1 col-md-1 col-md-offset-0">
+        <div class="row">
+            <img src="../../img/logo.png" class="logo">
+        </div>
+    </div>
+    <div class="col-xs-12">
+        <div class="row">
+            <?php include_once '../navs/manager.php'; ?>
+        </div>
+    </div>
+    <div class="col-xs-12">
+        <div class="row">
+            <h1>Welkom!</h1>
+        </div>
+    </div>
+</div>
+</body>
+</html>
