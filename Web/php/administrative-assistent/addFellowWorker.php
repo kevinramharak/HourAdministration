9e12ab65664c85d18e60e9a2244529a2e38ae0bd
KevinRamharak <kevinramharak_95@hotmail.com>
2016-03-23 17:33:57 +0100

    administrator navbar is af

---
+++ Web/php/administrative-assistent/addFellowWorker.php
@@ -0,0 +1,11 @@
+<html>
+<?php include_once '../head.php';?>
+<?php include_once '../navs/administrator.php';?>
+<div class="col-xs-12">
+  <div class="row">
+    <h1>Welkom!</h1>
+  </div>
+</div>
+</div>
+</body>
+</html>
---
+++ Web/php/navs/administrator.php
@@ -0,0 +1,39 @@
+<body>
+<div class="col-xs-12">
+    <div class="row">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="../fellow-worker/home.php"">HourAdministration</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="parameters.php">Parameters</a></li>
+                    <li><a href="setNextHolidayDays.php">Meenemen vakantiedagen aangeven</a></li>
+                    <li><a href="holidays.php">Feestdagen</a></li>
+                    <li><a href="fellowWorker.php">Werknemer overzicht</a></li>
+                    <li><a href="allFellowWorkers.php">Werknemers overzicht</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Instellingen <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="editFellowWorker.php">Werknemer aanpassen</a></li>
+                          <li><a href="addFellowWorker.php">Werknemer toevoegen</a></li>
+                          <li><a href="removeFellowWorker.php">Werknemer verwijderen</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+    </div>
+</divdiv>
+</div>
