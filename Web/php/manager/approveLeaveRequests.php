6456edfc750ce89a4deaf81127f905654eeeddc4
justusj94 <justusjansen94@gmail.com>
2016-03-23 18:02:45 +0100

    fak u daan

--- Web/php/manager/approveLeaveRequests.php
+++ Web/php/manager/approveLeaveRequests.php
@@ -2,11 +2,6 @@
 <?php include_once '../head.php'; ?>
 <body>
 <div class="container">
-    <div class="col-xs-10 col-xs-offset-1 col-md-1 col-md-offset-0">
-        <div class="row">
-            <img src="../../img/logo.png" class="logo">
-        </div>
-    </div>
     <div class="col-xs-12">
         <div class="row">
             <?php include_once '../navs/manager.php'; ?>
