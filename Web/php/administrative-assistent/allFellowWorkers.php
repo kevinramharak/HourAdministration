3ca52ac87660985aeb486d49dd33940376811edb
Tedderksen <t.derksen@hotmail.com>
2016-03-23 21:05:19 +0100

    teun

--- Web/php/administrative-assistent/allFellowWorkers.php
+++ Web/php/administrative-assistent/allFellowWorkers.php
@@ -3,10 +3,38 @@
 <div class="container">
   <?php include_once '../head.php';?>
   <?php include_once '../navs/administrator.php';?>
-  <div class="col-xs-12">
     <div class="row">
-      <h1>Welkom!</h1>
     </div>
+    <div class="panel panel-default">
+      <!-- Default panel contents -->
+      <div class="panel-heading">Overzicht werknemers</div>
+      <!-- Table -->
+      <table class="table">
+        <tr>
+          <th>Werknemer nummer</th>
+          <th>Voornaam</th>
+          <th>Tussenvoegsel</th>
+          <th>Achternaam</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
 </div>
 </body>
 </html>
--- Web/php/administrative-assistent/fellowWorker.php
+++ Web/php/administrative-assistent/fellowWorker.php
@@ -4,9 +4,60 @@
   <?php include_once '../head.php';?>
   <?php include_once '../navs/administrator.php';?>
   <div class="col-xs-12">
-    <div class="row">
-      <h1>Welkom!</h1>
-    </div>
+  </div>
+  <div class="row">
+  </div>
+  <div class="panel panel-default">
+    <!-- Default panel contents -->
+    <div class="panel-heading">Details werknemers</div>
+    <!-- Table -->
+    <table class="table">
+      <tr>
+        <th>Werknemer nummer</th>
+        <th>Voornaam</th>
+        <th>Tussenvoegsel</th>
+        <th>Achternaam</th>
+        <th>Geboortedatum</th>
+        <th>Deeltijdfactor</th>
+        <th>Opgenomen vrijedagen</th>
+        <th>Openstaande vrijedagen</th>
+        <th>Aantal ziektedagen</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+  </div>
   </div>
 </div>
 </body>
