708851a1021125eacb6165ca2031e1978c503319
justusj94 <justusjansen94@gmail.com>
2016-03-23 13:53:30 +0100

    inlogpagina

--- Web/php/log-in.php
+++ Web/php/log-in.php
@@ -1,6 +1,10 @@
 <?php include 'head.php'; ?>
 
-
-
-
+<div class="panel panel-default col-md-4 col-md-offset-4">
+    <div class="panel-body">
+        <input type="text" class="form-control" placeholder="Gebruikersnaam">
+        <input type="text" class="form-control" placeholder="Wachtwoord">
+        <button type="submit" class="btn btn-default">Inloggen</button>
+    </div>
+</div>
 
