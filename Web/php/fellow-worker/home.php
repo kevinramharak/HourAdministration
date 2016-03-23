878a2e12ab18d4ebf60bd09e10d5505e7f72102b
Daan <daan@daan-janssen.nl>
2016-03-23 14:47:08 +0100

    pages

---
+++ Web/php/fellow-worker/home.php
@@ -0,0 +1,11 @@
+<html>
+<?php include_once '../head.php'; ?>
+<body>
+    <div class="container">
+        <div class="col-xs-10 col-xs-offset-1 col-md-2">
+            <img src="../../img/logo.png" class="logo">
+        </div>
+        <?php include_once '../navs/fellowworker.php'; ?>
+    </div>
+</body>
+</html>
