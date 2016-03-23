57fe1fd7a246c8bae251885e963315576118b530
justusj94 <justusjansen94@gmail.com>
2016-03-23 17:27:36 +0100

    aanpassingen front frent

---
+++ Web/php/fellow-worker/overtime.php
@@ -0,0 +1,47 @@
+<html>
+<?php include_once '../head.php'; ?>
+<body>
+<div class="container">
+    <?php include '../navs/fellowworker.php'; ?>
+    <div class="col-xs-12">
+        <div class="row">
+            <h1 class="title">Overwerk registreren</h1>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-4 col-md-offset-4">
+                <div class="row">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Overwerk registreren</div>
+                        <div class="panel-body">
+                            <form>
+                                <div class="col-xs-12">
+                                    <div class="col-xs-12 col-md-6">
+                                        <label>Van:</label>
+                                    </div>
+                                    <div class="form-group col-xs-12 col-md-6">
+                                        <input type="time" class="col-md-7 form-control">
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="col-xs-12 col-md-6">
+                                        <label>Tot:</label>
+                                    </div>
+                                    <div class="form-group col-xs-12 col-md-6">
+                                        <input type="time" class="col-md-7 form-control">
+                                    </div>
+                                </div>
+                                <div class="col-xs-12">
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-primary">Ziek melden</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
