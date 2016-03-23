067197de5d7feea4aa4b4b26397eca2b84ba8d0e
Daan <daan@daan-janssen.nl>
2016-03-23 15:02:45 +0100

    jongens, we gaan (niet) winnen

--- Web/php/fellow-worker/home.php
+++ Web/php/fellow-worker/home.php
@@ -2,10 +2,22 @@
 <?php include_once '../head.php'; ?>
 <body>
     <div class="container">
-        <div class="col-xs-10 col-xs-offset-1 col-md-2">
-            <img src="../../img/logo.png" class="logo">
+        <div class="col-xs-10 col-xs-offset-1 col-md-1 col-md-offset-0">
+            <div class="row">
+                <img src="../../img/logo.png" class="logo">
+            </div>
         </div>
-        <?php include_once '../navs/fellowworker.php'; ?>
+        <div class="col-xs-12">
+            <div class="row">
+                <?php include_once '../navs/fellowworker.php'; ?>
+            </div>
+        </div>
+        <div class="col-xs-12">
+            <div class="row">
+                <h1>Welkom!</h1>
+            </div>
+        </div>
     </div>
+    <?php include_once 'body.php'; ?>
 </body>
 </html>
