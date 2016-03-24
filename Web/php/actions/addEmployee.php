5a6a278f02b461d7d9a43939a84deedc8f539137
KevinRamharak <kevinramharak_95@hotmail.com>
2016-03-24 06:30:15 +0100

    add employee werkend

--- Web/php/actions/addEmployee.php
+++ Web/php/actions/addEmployee.php
@@ -1,7 +1,20 @@
 <?php
 session_start();
-if(isset($_GET['']) && isset($_GET[''])){
+if(isset($_GET['first-name']) && isset($_GET['prefix'])&& isset($_GET['last-name'])&& isset($_GET['date-of-birth'])&& isset($_GET['sex'])&& isset($_GET['employee-number'])&& isset($_GET['factor-time'])){
 
+  require_once '../../../API/Employee.class.php';
+
+  $emp = new Employee();
+
+  $firstname = $_GET['first-name'];
+  $prefix = $_GET['prefix'];
+  $lastname = $_GET['last-name'];
+  $birthdate= $_GET['date-of-birth'];
+  $sex = $_GET['sex'];
+  $employeeid = $_GET['employee-number'];
+  $parttimefactor = $_GET['factor-time'];
+
+  $emp->register($firstname, $prefix, $lastname, $birthdate, $sex, $employeeid, $parttimefactor);
 
   header("Location: http://localhost/HourAdministration/Web/php/administrative-assistent/home.php");
 } else {
