804d2e1db5c926af4a8c3c138e130fc5146f1e0c
justusj94 <justusjansen94@gmail.com>
2016-03-24 04:30:53 +0100

    wat aanpassingen

--- Web/php/fellow-worker/vacation.php
+++ Web/php/fellow-worker/vacation.php
@@ -8,7 +8,7 @@
             <h1 class="title">Vakantie of verlof aanvragen</h1>
         </div>
         <div class="row">
-            <div class="col-xs-12 col-md-4 col-md-offset-4">
+            <div class="col-xs-12 col-md-6 col-md-offset-3">
                 <div class="row">
                     <div class="panel panel-default">
                         <div class="panel-heading">Vakantie of verlof aanvragen</div>
@@ -19,7 +19,7 @@
                                         <label>Van:</label>
                                     </div>
                                     <div class="form-group col-xs-12 col-md-8">
-                                        <input type="datetime-local" class="col-md-7 form-control" name="from">
+                                        <input type="datetime-local" class="col-md-8 form-control" name="from">
                                     </div>
                                 </div>
                                 <div class="col-xs-12">
@@ -27,7 +27,7 @@
                                         <label>Tot:</label>
                                     </div>
                                     <div class="form-group col-xs-12 col-md-8">
-                                        <input type="datetime-local" class="col-md-7 form-control" name="until">
+                                        <input type="datetime-local" class="col-md-8 form-control" name="until">
                                     </div>
                                 </div>
                                 <div class="col-xs-12">
